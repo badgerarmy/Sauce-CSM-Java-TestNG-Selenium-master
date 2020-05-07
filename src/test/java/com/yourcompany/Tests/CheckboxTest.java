75bb69d7edd422c077e4b84225e370a0207fead2
badgerarmy <megan@badgerarmy.com>
2020-05-07 00:37:03 -0500

    Update CheckboxTest.java

--- src/test/java/com/yourcompany/Tests/CheckboxTest.java
+++ src/test/java/com/yourcompany/Tests/CheckboxTest.java
@@ -37,7 +37,7 @@ public class CheckboxTest extends TestBase {
 
 
         this.annotate("Assert checkbox checked");
-        Assert.assertFalse(page.verifyCheckBox());
+        Assert.assertTrue(page.verifyCheckBox());
     }
 
 }
